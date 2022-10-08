208ba1563be2966a0a96d42785e265272458d851
Priyanshu Sharma <ps940016@gmail.com>
2022-10-08 21:53:57 +0530

    Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,11 @@ import { faCirclePlus} from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Header(props) {
-    // const [degree, setDegree] = useState(0);
-
-    // var rotation = {
-    //     transform: "rotate(" + degree + "deg)",
-    //     transition: "transform 0.4s ease"
-    // }
-
-    // function rotate() {
-    //     if(degree === 0) {
-    //         setDegree(45);
-    //     } else {
-    //         setDegree(0);
-    //     }
-    // }
-
     return (
-        <header className='
-            py-5 
-            px-2 
-            2xl:px-44 
-            xl:px-20
-            text-white 
-            bg-[#534340] flex'>
-            <h1 className='font-extralight text-[1.7rem] ml-4 xl:ml-0 overflow-hidden'>MyLibrary</h1>
+        <header className='py-5 px-2 2xl:px-44 xl:px-20 text-white bg-[#534340] flex'>
+            <a href="https://github.com/xtechilad/my-library" target="#">
+                <h1 className='font-extralight text-[1.7rem] ml-4 xl:ml-0 overflow-hidden'>MyLibrary</h1>
+            </a>
 
             <button id='addButton' className='ml-auto mr-4 xl:mr-0 bg-white hover:bg-gray-100 text-black font-bold py-2 px-4 rounded hidden sm:block' onClick={props.showForm}>Add a Book</button>
             <button className='ml-auto mr-4 text-black font-bold rounded-full visible sm:hidden' onClick={() => {props.showForm(); props.rotate()}}>
@@ -36,5 +17,3 @@ export default function Header(props) {
         </header>
     );
 }
-
-// Header light mode : #967E76
