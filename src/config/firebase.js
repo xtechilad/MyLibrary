4c13b87e732c64e4c107737ac70cd2f715d47eae
xtechilad <ps940016@gmail.com>
2022-11-11 14:28:20 +0530

    Delete .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: `${process.env.REACT_APP_API_KEY}`,
-    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-    appId: `${process.env.REACT_APP_APP_ID}`
+    apiKey: "AIzaSyDjq9HSvpNdqdfYSg37VpZJ4dwcTbnfrHQ",
+    authDomain: "my-library-1d89b.firebaseapp.com",
+    projectId: "my-library-1d89b",
+    storageBucket: "my-library-1d89b.appspot.com",
+    messagingSenderId: "166461716416",
+    appId: "1:166461716416:web:e8a0b774ea884d7d937a25"
 };
 
 // Initialize Firebase
